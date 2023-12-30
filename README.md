b444431dd7d588b4d152a2f23ced63d326dc83ff
Jakob Alander <jakob.alander@gmail.com>
2023-12-31 00:23:55 +0100

    Add boost string algorithm example.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Template repository for c++ builds with cmake.
 
 ![build status](https://github.com/jakobalander/cpp-cmake-template/actions/workflows/cmake-build-and-test.yml/badge.svg)
+
+`git submodule update --init --recursive`
+
+`cmake -S . -B build`
--- apps/skeleton.cpp
+++ apps/skeleton.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
+#include <boost/algorithm/string/case_conv.hpp>
 #include "skeleton/lib.hpp"
 
 int main()
 {
-    std::cout << "this is my fancy app that uses a library\n";
+    std::string s{"this is my fancy app that uses a library"};
+    boost::algorithm::to_upper(s);
+    std::cout << s << "\n";
     Skeleton skel{};
     skel.greeting();
 }
